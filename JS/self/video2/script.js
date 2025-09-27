1abf639ab159f123463cf7e886be41e7d7f18de9
Alok Maurya <alokmaurya.dev@gmail.com>
2025-09-27 23:26:01 +0530

    done some changes to gallery project

---
+++ JS/self/video2/script.js
@@ -0,0 +1,47 @@
+console.log("hey this is tutorial 55")
+
+var a = 5;
+var b = 6;
+var c = "Harry";
+
+console.log(a + b + 8);
+
+console.log(typeof a, typeof b, typeof c);
+
+// const a1 = 6;
+// a1 = 6 + 1
+
+// will not work
+
+{
+
+    // let is blocked scope
+    let a = 55
+    console.log(a)
+}
+
+console.log(a)
+
+let x = "harry bhai";
+let y = 22;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x,y,z,p,q,r)
+console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r)
+
+let o ={
+    name:"harry",
+    "job role":"sde 2",
+}
+
+console.log(o);
+
+o["salary amount"] = "100 crores";
+console.log(o)
+o["salary amount"] = "500 crores";
+console.log(o)
+o.isHandsome = true;
+console.log(o)
