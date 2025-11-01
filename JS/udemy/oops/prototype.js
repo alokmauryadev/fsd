f4b44511c24bc38ca73ad9d10a7b3648de12bfb1
Alok Maurya <alokmaurya.dev@gmail.com>
2025-11-01 12:02:13 +0530

    sheriyan coding school

--- JS/udemy/oops/prototype.js
+++ JS/udemy/oops/prototype.js
@@ -1,16 +1,17 @@
-let computer = {cpu:12};
-let lenovo = {screen:"HD",
-    __proto__:computer,
-}
+let computer = { cpu: 12 };
+let lenovo = {
+  screen: "HD",
+  __proto__: computer,
+};
 let tomHardware = {};
 console.log(`lenovo`, lenovo.__proto__);
 
-let genericCar = {tyres:4}
+let genericCar = { tyres: 4 };
 
 let tesla = {
-    driver: "AI"
-}
-Object.setPrototypeOf(tesla,genericCar)
+  driver: "AI",
+};
+Object.setPrototypeOf(tesla, genericCar);
 
-console.log(`tesla`, Object.getPrototypeOf(tesla))
-console.log(`tesla`,(tesla.tyres))
+console.log(`tesla`, Object.getPrototypeOf(tesla));
+console.log(`tesla`, tesla.tyres);
